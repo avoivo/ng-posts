8b6bdd04694e0c9c99246f48e0f4a5f5e6e5d85f
Athanasios Voivodas <tvoivodas@gmail.com>
2018-03-24 17:34:45 +0200

    Adds bootstrap styling.

--- src/app/core/containers/app/app.component.ts
+++ src/app/core/containers/app/app.component.ts
@@ -1,8 +1,20 @@
 import { Component } from "@angular/core";
+import { Router, NavigationEnd } from "@angular/router";
+
+import { Observable } from "rxjs/Observable";
+import { filter, map } from "rxjs/operators";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"]
 })
-export class AppComponent {}
+export class AppComponent {
+  isHome$: Observable<boolean>;
+  constructor(private router: Router) {
+    this.isHome$ = this.router.events.pipe(
+      filter(e => e instanceof NavigationEnd),
+      map((e: NavigationEnd) => e.url.endsWith("/"))
+    );
+  }
+}
